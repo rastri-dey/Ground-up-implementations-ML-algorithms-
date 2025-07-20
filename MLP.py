ff2ac278f99133ea85891c7a2d77573b0491a515
rastri-dey <rastri@umich.edu>
2025-07-20 16:28:04 -0700

    MLP

---
+++ MLP.py
@@ -0,0 +1,5 @@
+# Define the model (Layers)
+# Compile the model (Optimizer, loss function)
+# Fit the model (trained data, epochs)
+# Calculate the accuracy for train data, test data
+# Predict the model (test data)
