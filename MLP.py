4961281456db44e557cc4f7d44ec86fedcc8b185
rastri-dey <rastri@umich.edu>
2025-07-21 19:35:38 -0700

    MLP

--- MLP.py
+++ MLP.py
@@ -1,5 +1,8 @@
+## MLP Tensorflow
 # Define the model (Layers)
 # Compile the model (Optimizer, loss function)
 # Fit the model (trained data, epochs)
 # Calculate the accuracy for train data, test data
 # Predict the model (test data)
+ 
+## MLP Pytorch
