d200af47fb2391bcf8237973a94a62c74e06a162
rastri-dey <rastri@umich.edu>
2025-07-28 06:44:49 -0700

    MLP - Pytorch

--- MLP.py
+++ MLP.py
@@ -6,3 +6,13 @@
 # Predict the model (test data)
 
 ## MLP Pytorch
+# Import Libraries
+# Get dataset - train loader, validation loader from data loader
+# Define the model
+# Define the Optimizer, Loss fun, epochs
+# Compile the model for all epochs and for all batches:
+    # Fit the model - Get the ouput (logits) for the current batch, current epoch
+    # Get the loss
+    # Zeros the gradient of model parameters
+    # Backpropagation (calculate backward loss)
+    # Update weights using Optimizer
