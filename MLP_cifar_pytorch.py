6a4eddd8b8f186fe33462c9d9c9e57534ae8d376
rastri-dey <rastri@umich.edu>
2025-08-03 10:17:35 -0700

    Multilayer Perceptron on CIFAR10 using Pytorch

--- MLP_cifar_pytorch.py
+++ MLP_cifar_pytorch.py
@@ -2,6 +2,18 @@
 ML Algorithm: Multilayer Perceptron
 Deep Learning Framework: Pytorch
 Dataset: CIFAR10
+
+Steps:
+# Import Libraries
+# Get dataset - train loader, validation loader from data loader
+# Define the model
+# Define the Optimizer, Loss fun, epochs
+# Compile the model for all epochs and for all batches:
+    # Fit the model - Get the ouput (logits) for the current batch, current epoch
+    # Get the loss
+    # Zeros the gradient of model parameters
+    # Backpropagation (calculate backward loss)
+    # Update weights using Optimizer
 '''
 
 ## Import Libraries
--- MLP_mnist_tf.py
+++ MLP_mnist_tf.py
@@ -1,18 +1,14 @@
-## MLP Tensorflow
-# Define the model (Layers)
-# Compile the model (Optimizer, loss function)
+'''
+ML Algorithm: Multilayer Perceptron
+Deep Learning Framework: Tensorflow
+Dataset: MNIST
+
+Steps:
+# Import Libraries
+# Get dataset - train, validation data
+# Define the model (Layers, activation)
+# Compile the model (Optimizer, Loss function)
 # Fit the model (trained data, epochs)
 # Calculate the accuracy for train data, test data
 # Predict the model (test data)
-
-## MLP Pytorch
-# Import Libraries
-# Get dataset - train loader, validation loader from data loader
-# Define the model
-# Define the Optimizer, Loss fun, epochs
-# Compile the model for all epochs and for all batches:
-    # Fit the model - Get the ouput (logits) for the current batch, current epoch
-    # Get the loss
-    # Zeros the gradient of model parameters
-    # Backpropagation (calculate backward loss)
-    # Update weights using Optimizer
+'''
