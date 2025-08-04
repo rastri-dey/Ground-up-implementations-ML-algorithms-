4295378e301d1e4dd4066e4e3f2c616cc1d3a809
rastri-dey <rastri@umich.edu>
2025-08-04 07:00:51 -0700

    Multilayer Perceptron on MNIST using Tensorflow-Keras

--- MLP_mnist_tf.py
+++ MLP_mnist_tf.py
@@ -13,3 +13,25 @@ Steps:
 # Predict the model (test data)
 '''
 
+## Import the libraries
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+## Get the dataset - MNIST
+mnist = keras.datasets.mnist
+(train_images, train_labels),(test_images, test_labels) = mnist.load_data()
+'''
+A tuple of 2 tuples, each tuple containing 2 NumPy arrays
+'''
+## Define the model
+
+model = keras.Sequential(
+    [
+        keras.layers.Flatten(input_shape=(28,28)),
+        keras.layers.Dense(128, activation = tf.nn.relu),
+        keras.layers.Dense(128, activation = tf.nn.relu),
+        keras.layers.Dense(10, activation = tf.nn.softmax)
+    ]
+)
+model.summary()
